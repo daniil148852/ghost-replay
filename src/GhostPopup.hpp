5d9546471497172e68aa68058d8bcacaae632f01
daniil148852 <daniildan850@gmail.com>
2026-03-01 15:59:56 +0100

    Update GhostPopup.hpp

--- src/GhostPopup.hpp
+++ src/GhostPopup.hpp
@@ -1,10 +1,49 @@
 #pragma once
 
+#include "FileManager.hpp"
 #include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
+#include <Geode/ui/ScrollLayer.hpp>
 
 using namespace geode::prelude;
 
-class GhostPopup : public FLAlertLayer {
+class GhostPopup : public geode::Popup<int> {
 public:
-    // TODO: Implement
+    static GhostPopup* create(int levelID);
+    
+protected:
+    bool setup(int levelID) override;
+    
+    void onClose(CCObject*) override;
+    
+private:
+    int m_levelID;
+    ScrollLayer* m_scrollLayer = nullptr;
+    CCMenu* m_listMenu = nullptr;
+    std::vector<RecordFileInfo> m_records;
+    
+    void refreshList();
+    CCNode* createRecordCell(const RecordFileInfo& info, int index);
+    
+    void onToggleRecord(CCObject* sender);
+    void onDeleteRecord(CCObject* sender);
+    void onDeleteAll(CCObject* sender);
+    void onRefresh(CCObject* sender);
+};
+
+// Попап для выбора уровня (из главного меню)
+class GhostMainPopup : public geode::Popup<> {
+public:
+    static GhostMainPopup* create();
+    
+protected:
+    bool setup() override;
+    
+private:
+    ScrollLayer* m_scrollLayer = nullptr;
+    
+    void refreshList();
+    CCNode* createLevelCell(int levelID, const std::string& levelName, int recordCount);
+    
+    void onSelectLevel(CCObject* sender);
 };
