9147368b0149ef84eec37dfa2b79a00d2f3a6d6a
daniil148852 <daniildan850@gmail.com>
2026-03-01 16:02:37 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <Geode/Geode.hpp>
+#include <Geode/loader/SettingEvent.hpp>
+#include "FileManager.hpp"
+#include "Recorder.hpp"
+#include "GhostManager.hpp"
 
 using namespace geode::prelude;
 
 $on_mod(Loaded) {
     log::info("Ghost Replay mod loaded!");
+    
+    // Инициализация файлового менеджера
+    FileManager::get()->init();
+    
+    log::info("Records directory: {}", FileManager::get()->getRecordsDir().string());
+}
+
+$on_mod(Unloaded) {
+    log::info("Ghost Replay mod unloaded!");
+    
+    // Останавливаем запись если идёт
+    if (Recorder::get()->isRecording()) {
+        Recorder::get()->stopRecording(nullptr);
+    }
+    
+    // Очищаем призраков
+    GhostManager::get()->onExitLevel();
 }
